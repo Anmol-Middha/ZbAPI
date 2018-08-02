64f07f40aad9ab8ce66ad6d001f514bf60d15c57
Anmol Middha <anmolmiddha2827@gmail.com>
2018-08-02 14:29:30 +0530

    CORS enabled

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static('public'));
 
 app.get('/',function(req,res){
